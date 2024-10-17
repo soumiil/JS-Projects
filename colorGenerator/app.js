28deea70dc16d529b6a2785e8862c73c03725432
Soumil Agarwal <agarwalsoumil6@gmail.com>
2024-10-17 22:20:37 +0530

    fe

--- colorGenerator/app.js
+++ colorGenerator/app.js
@@ -2,11 +2,11 @@ let body = document.querySelector("body");
 const button = document.getElementById("btn");
 let hexCode = document.getElementById("hexCode");
 let color = "#";
-const colorString = "0123456789ABCDEF";
+const hexString = "0123456789ABCDEF";
 
 function colorGenerator() {
     for (let i = 0; i < 6; i++) {
-        color += colorString[Math.floor(Math.random() * colorString.length)];
+        color += hexString[Math.floor(Math.random() * hexString.length)];
     }
     hexCode.innerText = color;
     body.style.backgroundColor = color;
